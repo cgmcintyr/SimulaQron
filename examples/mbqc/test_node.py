6b3d66c95cbff17b16c25cedcaeb130b5703d051
Marc <marc.ibrahim@gmail.com>
2018-10-14 12:29:48 +0300

    add test node

---
+++ examples/mbqc/test_node.py
@@ -0,0 +1,18 @@
+from SimulaQron.general.hostConfig import *
+from SimulaQron.cqc.backend.cqcHeader import *
+from SimulaQron.cqc.pythonLib.cqc import *
+import numpy as np
+import random
+#from SimulaQron.virtnode.crudeSimulator.simpleEngine import *
+
+# Initialize the connection
+with CQCConnection("client") as client:
+
+	
+	rand_angle=int(256*random.random())
+	q = qubit(client)
+	q.rot_Y(64) #|+> state
+	q.rot_Z(rand_angle)
+	m=q.measure()
+	print(m)
+		
